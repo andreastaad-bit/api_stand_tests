e8577d7a4635b5df17399706411aa4da956229ff
Andrea Guerra <andreastaad@gmail.com>
2026-04-04 18:01:26 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-b68272e3-9b54-487e-aef2-2901939d113b.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/",
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ data.py
@@ -0,0 +1,8 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {"firstName": 12,
+             "phone": "+1234567890",
+             "address": "123 Elm Street, Hilltop"
+             }
